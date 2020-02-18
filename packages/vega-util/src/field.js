17034279c03f6fa2d05983b88d03e51e400680f5
Chris Game <chris.game@crowdstrike.com>
2020-02-18 10:37:33 +0000

    remove Function usage from vega-util field

--- packages/vega-util/src/field.js
+++ packages/vega-util/src/field.js
@@ -1,13 +1,24 @@
 import accessor from './accessor';
 import splitAccessPath from './splitAccessPath';
-import stringValue from './stringValue';
+import stringValue from './stringValue'
 
 export default function(field, name) {
-  var path = splitAccessPath(field),
-      code = 'return _[' + path.map(stringValue).join('][') + '];';
+  var path = splitAccessPath(field);
+  var fn = function(p) {
+    let path = p.map(s => stringValue(s));
+
+    return function anonymous(_) {
+      return path.reduce(function(acc, value) {
+        let number = JSON.parse(value);
+        let isNumber = typeof(number === 'number');
+
+        return isNumber ? Reflect.get(acc, [number]) : Reflect.get(acc, value);
+      }, _);
+    };
+  };
 
   return accessor(
-    Function('_', code),
+    fn(path),
     [(field = path.length===1 ? path[0] : field)],
     name || field
   );
